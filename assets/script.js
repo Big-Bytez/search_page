0eb96e0889bb42f9375ea03c392de02165934ba1
PederDDS <pederdd@stud.ntnu.no>
2021-11-11 18:38:43 -0800

    minor changes

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,19 @@ function init(){
         let ele = document.createElement('div');
         let numb = rec % 3;
         ele.setAttribute('class',`type${numb}`);
-        ele.innerHTML = `Recipe ${rec+1}`;
+        let images = document.createElement('img')
+        images.setAttribute('src',"images/pasta.jpeg")
+        images.setAttribute('alt',"shuffle-button")
+        images.setAttribute('alt',"shuffle-button")
+        images.setAttribute('title',"Random-Recipe")
+        let recipe_title = document.createElement('p')
+        recipe_title.setAttribute('class',"recipe_title")
+        recipe_title.innerHTML = "Delish Delish";
+
+        
+        ele.appendChild(images);
+        ele.appendChild(recipe_title);
+        //ele.innerHTML = `Recipe ${rec+1}`;
         document.getElementsByClassName('mini-recipes')[0].appendChild(ele);
         
     }
